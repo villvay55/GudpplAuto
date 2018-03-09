29656e1a0c1b935aaa50451829241529961c83f6
vijay <vijay@DESKTOP-BADJCCG>
2018-03-09 13:06:29 -0800

    sign

--- src/main/java/com/gudppl/signup/Pages/signUpLandPage.java
+++ src/main/java/com/gudppl/signup/Pages/signUpLandPage.java
@@ -2,6 +2,7 @@ package com.gudppl.signup.Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class signUpLandPage {
 
@@ -16,15 +17,62 @@ public class signUpLandPage {
 		this.driver = driver;
 	}
 	
-	public signUpPage signLand(){
+	public signUpPage signLand() throws InterruptedException{
 		
+		ClickOnSignUpLnk();
 		
+		synchronized (driver)
+		{
+		    driver.wait(3000);
+		}
 		
+		ClickOnSignUpEmail();
+		
+		synchronized (driver)
+		{
+		    driver.wait(3000);
+		}
 		
 		return new signUpPage(driver);
 	
 	}
 	
+	
+	public void ClickOnSignUpLnk() {
+		
+		WebElement signUpLnkElement  = driver.findElement(signUpLnk);
+	
+		if(signUpLnkElement.isDisplayed()) {
+			
+			signUpLnkElement.click();
+			
+		}
+		
+	}
+	
+	public void ClickOnSignUpEmail() {
+		
+		WebElement singUpEmailElement  = driver.findElement(singUpEmail);
+	
+		if(singUpEmailElement.isDisplayed()) {
+			
+			singUpEmailElement.click();
+			
+		}
+		
+	}
+	
+	public void ClickOnSignUpFaceBook() {
+		
+		WebElement singUpFaceBookElement  = driver.findElement(singUpFaceBook);
+	
+		if(singUpFaceBookElement.isDisplayed()) {
+			
+			singUpFaceBookElement.click();
+			
+		}
+		
+	}
 	
 	
 }
